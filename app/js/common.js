94439cdffe9a1507de89e7478731bea1d2393a01
“ShoutingLime” <shouting.lime@gmail.com>
2018-11-18 13:31:30 +0300

    Добавил обработчик кликов на поля через делегирование

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,7 @@
   const tds = Array.prototype.slice.call(document.querySelectorAll('#playsquare td > input'));
   const nouns = ["балда", "дурак", "пурга", "сосед", "кабан", "диван", "топор", "пирог", "комод", "ясень"];
   const rand = Math.floor(Math.random() * nouns.length);
+  const playsquare = document.querySelector('#playsquare');
   console.log(nouns[rand]);
   console.log(tds);
   for(let i = 0; i < tds.length; i++) {
@@ -9,4 +10,30 @@
       tds[i].value = nouns[rand][((i + 3) / 5) - 1];
     }
   }
+
+  // Обработка кликов с делегированием
+  let selectedTd;
+  playsquare.onclick = function(event) {
+    let target = event.target;
+
+    // цикл двигается вверх от target к родителям до table
+    while (target !== playsquare) {
+      if (target.tagName === 'TD') {
+        // нашли элемент, который нас интересует!
+        highlight(target);
+        return;
+      }
+      target = target.parentNode;
+    }
+  };
+
+  function highlight(node) {
+    if (selectedTd) {
+      selectedTd.classList.remove('highlight');
+    }
+    selectedTd = node;
+    selectedTd.classList.add('highlight');
+  }
 }());
+
+
